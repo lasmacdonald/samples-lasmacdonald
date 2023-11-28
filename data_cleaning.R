b5c3a6f2cdee9e15ec5bb600dea305e7bed3ec29
lasmacdonald <lasmacdonald@gmail.com>
2023-11-28 05:00:58 +0000

    Revised homework assignment 04

--- data_cleaning.R
+++ data_cleaning.R
@@ -7,6 +7,7 @@ samples <- read_excel("TS_poo_2022.xlsx")
 glimpse(samples)
 
 samples |> 
+  rename(sample_date = Sample_Date) |> 
   mutate(id = 1:n()) |> 
   relocate(id) |> 
-  relocate(Sample_Date, .before = source_location) 
+  relocate(sample_date, .before = source_location) 
